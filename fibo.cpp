bfa419fc43e61686baeef345365bf25bb9889bf5
N3T <adrian.adek@gmail.com>
2014-04-18 11:58:05 +0200

    Added fibo.cpp

---
+++ fibo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int fibo(unsigned long int n) {
+  if(n == 1) {
+    return 1;
+  }
+  if(n == 2) {
+    return 1;
+  }
+  return fibo(n-1) + fibo(n-2);
+}
+
+int main() {
+  unsigned long int n;
+  std::cout << "Ktora liczbe ciagu Fibonancciego mam wypisac? ";
+  std::cin >> n;
+  std::cout << n << ". liczba ciagu Fibonnaciego to " << fibo(n) << std::endl;
+  return 0;
+}
