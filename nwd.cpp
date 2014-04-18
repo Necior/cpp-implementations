c9a5b96c79dcb746187bd908e41a88a07790354f
N3T <adrian.adek@gmail.com>
2014-04-18 09:57:35 +0200

    Added command line arguments handling

--- nwd.cpp
+++ nwd.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 
 int nwd(int a, int b) { // assuming (a > 0) and (b > 0)
   int c = 0; // initial value doesn't matter
@@ -10,7 +11,26 @@ int nwd(int a, int b) { // assuming (a > 0) and (b > 0)
   return a;
 }
 
-int main() { // usage example
-  std::cout << nwd(12, 8) << std::endl;
+int main(int argc, char* argv[]) {
+  int a = 12;
+  int b = 8;
+  if(argc >= 3) {
+    std::istringstream sa(argv[1]);
+    if(!(sa >> a)) {
+      std::cerr << argv[1] << " is not a number." << std::endl;
+      return -1;
+    }
+    std::istringstream sb(argv[2]);
+    if(!(sb >> b)) {
+      std::cerr << argv[2] << " is not a number." << std::endl;
+      return -1;
+    }
+    std::cout << nwd(a, b) << std::endl;
+  }
+  else {
+    std::cout << "No command line arguments. Running sample." << std::endl;
+    std::cout << "Result for a = " << a << " and b = " << b << ":" << std::endl;
+    std::cout << nwd(12, 8) << std::endl;
+  }
   return 0;
 }
